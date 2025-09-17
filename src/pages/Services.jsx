3ce63bce2cb5bc480907f9af5484992382ad637d
TUYISENGE Emmanuel <tuyisengeemmanuel1999@gmail.com>
2025-09-17 18:26:18 +0200

    same changes link on services

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -56,7 +56,7 @@ const Services = () => {
           {!isLoading && !error && transformedServices.length > 0 && (
              <div className="services-grid">
               {transformedServices.map((service, index) => (
-                <a href="/Services?id={index}">
+                <a href={`/Services?id=${service.description}`}>
                 <ServiceCard key={index} {...service} /></a>
               ))}
             </div>
