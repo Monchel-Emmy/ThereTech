e448010ae2fc146c531a32566a2f456068ca3603
TUYISENGE Emmanuel <tuyisengeemmanuel1999@gmail.com>
2025-09-20 23:55:42 +0200

    analytics addition

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,9 +2,12 @@ import NavBar from "./components/navBar";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
 
+import { Analytics } from "@vercel/analytics/next"
+
 const Layout = () => {
   return (
     <div className="layout">
+      <Analytics/>
       <NavBar />
       <main className="main-content">
         <Outlet />
