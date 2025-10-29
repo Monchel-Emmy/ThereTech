2ae5c6ce8c4553ecd5a7d46810576bb2ee453fa2
IGIRIMPUHWE Dositha <igirimpuhwedosta@gmail.com>
2025-10-29 10:17:30 +0300

    adding the SEO

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -125,7 +125,7 @@ const Contact = () => {
 
   return (
     <>
-    <SEO/>
+    <SEO title="ThereTech||Contact page" name="Contact page" content="contact page of ThereTech"/>
     <div className=" min-h-screen bg-gradient-to-br from-slate-50 to-blue-100">
     
 
